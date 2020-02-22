b696b4883e46bca7607d4576d34264f1cdcff8aa
Andrei Bostan <abostan.ir@gmail.com>
2020-02-22 19:13:38 +0100

    Version 1.0.0

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "client/build")));
 
 app.use(function(req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
