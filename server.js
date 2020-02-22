5e4264fdb8fc0256e2f591e97a8fd00e345b4815
Andrei Bostan <abostan.ir@gmail.com>
2020-02-22 20:29:57 +0100

    made get for *

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "client/build")));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "build", "index.html"));
+});
+
 app.use(function(req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
 
